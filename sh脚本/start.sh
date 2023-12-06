b826cee0470ce7465ed4f2b59058fa45e2a71cb1
root <583188551@qq.com>
2023-12-06 17:47:36 +0800

    Site updated: 2023-12-06 17:47:33

--- sh脚本/start.sh
+++ sh脚本/start.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-echo "进入指定git目录";
+#!/usr/bin/env bash
+echo "进入源文件-git目录";
+cd /home/AsummerCatHexo;
+echo "拉取最新代码";
+sleep 2s;
+sudo git pull;
+echo "进行hexo 操作";
+hexo clean && hexo g && hexo d
+sleep 2s;
+echo "进入编译-git目录";
 cd /home/AsummerCat.github.io;
 echo "拉取最新代码";
 sudo git pull;
